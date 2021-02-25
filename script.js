f16c8efbb76a14ad631b67af329c2a38b3909dc6
Ben Iverson <2513iverson989@gmail.com>
2021-02-25 11:13:36 -0600

    Created computerPlay function

---
+++ script.js
@@ -0,0 +1,17 @@
+// begin with a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+function computerPlay(){
+  let choice = Math.floor(Math.rand() * 3) + 1;
+  // rock = 1; paper = 2; scissors = 3;
+  // if choice === 1
+  if (choice === 1){
+    // return `rock`
+    return `rock`;
+  // else if choice === 2
+  } else if (choice === 2){
+    // return `paper`
+    return `paper`;
+  }
+  // if choice is neither 1 or 2
+  // return `scissors`
+  return `scissors`;
+}
