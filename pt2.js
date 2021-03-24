6833798f6c741f8cb17c21a93ef8ffbd43b7429c
Ben Iverson <2513iverson989@gmail.com>
2021-03-24 17:17:40 -0500

    severe clean-up of code structure

--- pt2.js
+++ pt2.js
@@ -4,6 +4,50 @@ document.body.style.flexWrap = "wrap";
 document.body.style.width = "420px";
 document.body.style.marginTop = "50vh";
 
+const rock = document.createElement("BUTTON");
+rock.textContent = "Rock";
+rock.className = "button";
+rock.setAttribute("id", "rock");
+document.body.appendChild(rock);
+const rockButton = document.getElementById("rock");
+rockButton.style.margin = "10px";
+rockButton.style.color = "red";
+rockButton.addEventListener("click", () => {
+  playRound(rockButton.textContent, computerPlay());
+});
+
+const paper = document.createElement("BUTTON");
+paper.textContent = "Paper";
+paper.className = "button";
+paper.setAttribute("id", "paper");
+document.body.appendChild(paper);
+const paperButton = document.getElementById("paper");
+paperButton.style.margin = "10px";
+paperButton.style.color = "white";
+paperButton.addEventListener("click", () => {
+  playRound(paperButton.textContent, computerPlay());
+});
+
+const scissors = document.createElement("BUTTON");
+scissors.textContent = "Scissors";
+scissors.className = "button";
+scissors.setAttribute("id", "scissors");
+document.body.appendChild(scissors);
+const scissorsButton = document.getElementById("scissors");
+scissorsButton.style.margin = "10px";
+scissorsButton.style.color = "dodgerblue";
+scissorsButton.addEventListener("click", () => {
+  playRound(scissorsButton.textContent, computerPlay());
+});
+
+const div = document.createElement("div");
+div.setAttribute("id", "score");
+document.body.appendChild(div);
+const scoreCard = document.querySelector("#score");
+scoreCard.textContent = "Play 5 rounds to decide the Victor";
+scoreCard.style.backgroundColor = "black";
+scoreCard.style.color = "mistyrose";
+
 function computerPlay() {
   let choice = Math.floor(Math.random() * 3) + 1;
   if (choice === 1) {
@@ -36,51 +80,3 @@ function playRound(playerSelection, computerSelection) {
   alert("loss");
   return;
 }
-
-const rock = document.createElement("BUTTON");
-rock.textContent = "Rock";
-rock.className = "button";
-rock.setAttribute("id", "rock");
-document.body.appendChild(rock);
-const rockButton = document.getElementById("rock");
-
-const paper = document.createElement("BUTTON");
-paper.textContent = "Paper";
-paper.className = "button";
-paper.setAttribute("id", "paper");
-document.body.appendChild(paper);
-const paperButton = document.getElementById("paper");
-
-const scissors = document.createElement("BUTTON");
-scissors.textContent = "Scissors";
-scissors.className = "button";
-scissors.setAttribute("id", "scissors");
-document.body.appendChild(scissors);
-const scissorsButton = document.getElementById("scissors");
-
-const div = document.createElement("div");
-div.setAttribute("id", "score");
-document.body.appendChild(div);
-const scoreCard = document.querySelector("#score");
-
-rockButton.style.margin = "10px";
-rockButton.style.color = "red";
-paperButton.style.margin = "10px";
-paperButton.style.color = "white";
-scissorsButton.style.margin = "10px";
-scissorsButton.style.color = "dodgerblue";
-scoreCard.textContent = "Play 5 rounds to decide the Victor";
-scoreCard.style.backgroundColor = "black";
-scoreCard.style.color = "mistyrose";
-
-rockButton.addEventListener("click", () => {
-  playRound(rockButton.textContent, computerPlay());
-});
-
-paperButton.addEventListener("click", () => {
-  playRound(paperButton.textContent, computerPlay());
-});
-
-scissorsButton.addEventListener("click", () => {
-  playRound(scissorsButton.textContent, computerPlay());
-});
