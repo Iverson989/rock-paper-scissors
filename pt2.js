7d69c5a1afb1b2898f698f319feda95123140f3f
Ben Iverson <2513iverson989@gmail.com>
2021-03-24 10:37:29 -0500

    Removed "game" function, "Round" will work for now

--- pt2.js
+++ pt2.js
@@ -25,38 +25,3 @@ function playRound(playerSelection, computerSelection) {
   }
   return `loss`;
 }
-
-function game() {
-  var result = {
-    win: 0,
-    loss: 0,
-    tie: 0,
-  };
-  for (var game = 0; game < 5; game++) {
-    let p1 = prompt(`What will you choose?`);
-    let roundResult = playRound(p1, computerPlay());
-    let tieMessage = `It's a Tie!`;
-    let winMessage = `You Win!`;
-    let lossMessage = `You Lose!`;
-    if (roundResult === `win`) {
-      console.log(winMessage);
-      result.win++;
-    } else if (roundResult === `loss`) {
-      console.log(lossMessage);
-      result.loss += 1;
-    } else if (roundResult === `tie`) {
-      console.log(tieMessage);
-      result.tie = result.tie + 1;
-    }
-  }
-  if (result.win > result.loss) {
-    console.log(`YOU'RE A WINNER!!!\n${JSON.stringify(result)}`);
-  } else if (result.win < result.loss) {
-    console.log(`YOU JUST LOST THE GAME!\n${JSON.stringify(result)}`);
-  } else {
-    console.log(
-      `IT WAS A TIE, PLAY AGAIN TO SETTLE THIS!!!\n${JSON.stringify(result)}`
-    );
-  }
-  return `TO PLAY AGAIN: select console, then press UP ARROW and press ENTER`;
-}
